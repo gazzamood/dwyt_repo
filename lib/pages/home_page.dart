f34e340f7e01f7f3861f6ca615c2780a4db1f2dc
andrea <andreagazze98@gmail.com>
2024-07-10 17:48:15 +0200

    divisione pagina home da main

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,117 @@
+import 'package:dwyt_test/pages/allerta_page.dart';
+import 'package:dwyt_test/pages/attivita_page.dart';
+import 'package:dwyt_test/pages/informativa_page.dart';
+import 'package:dwyt_test/pages/login_page.dart';
+import 'package:dwyt_test/pages/notifica_page.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('DWYT APP'),
+        centerTitle: true,
+        backgroundColor: Colors.blue,
+        leading: IconButton(
+          icon: const Icon(Icons.account_circle),
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => const LoginPage()),
+            );
+          },
+        ),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.notifications),
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => const NotificaPage()),
+              );
+            },
+          ),
+        ],
+      ),
+      body: Column(
+        children: <Widget>[
+          Expanded(
+            child: Container(
+              margin: const EdgeInsets.all(16.0),
+              width: double.infinity,
+              child: ElevatedButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => const AllertaPage()),
+                  );
+                },
+                style: ElevatedButton.styleFrom(
+                  shape: const RoundedRectangleBorder(
+                    borderRadius: BorderRadius.zero,
+                  ),
+                  padding: const EdgeInsets.all(16.0),
+                ),
+                child: const Text(
+                  'Allerta',
+                  style: TextStyle(fontSize: 24.0),
+                ),
+              ),
+            ),
+          ),
+          Expanded(
+            child: Container(
+              margin: const EdgeInsets.all(16.0),
+              width: double.infinity,
+              child: ElevatedButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => const InformativaPage()),
+                  );
+                },
+                style: ElevatedButton.styleFrom(
+                  shape: const RoundedRectangleBorder(
+                    borderRadius: BorderRadius.zero,
+                  ),
+                  padding: const EdgeInsets.all(16.0),
+                ),
+                child: const Text(
+                  'Informativa',
+                  style: TextStyle(fontSize: 24.0),
+                ),
+              ),
+            ),
+          ),
+          Expanded(
+            child: Container(
+              margin: const EdgeInsets.all(16.0),
+              width: double.infinity,
+              child: ElevatedButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => const AttivitaPage()),
+                  );
+                },
+                style: ElevatedButton.styleFrom(
+                  shape: const RoundedRectangleBorder(
+                    borderRadius: BorderRadius.zero,
+                  ),
+                  padding: const EdgeInsets.all(16.0),
+                ),
+                child: const Text(
+                  'Attività',
+                  style: TextStyle(fontSize: 24.0),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
