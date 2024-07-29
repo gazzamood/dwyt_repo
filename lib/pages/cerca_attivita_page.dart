23a9c3d95700f120e9a3886652a240e161fbf592
andrea <andreagazze98@gmail.com>
2024-07-29 12:07:15 +0200

    fix add radius

--- lib/pages/cerca_attivita_page.dart
+++ lib/pages/cerca_attivita_page.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'details_acttivity_page.dart';
+import 'details_activity_page.dart';
 import 'map_page.dart';  // Ensure the import is correct
 
 class CercaAttivitaPage extends StatefulWidget {
