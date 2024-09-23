a21825efbddc3143f30b05034e4b0e6cbf81a687
andrea <andreagazze98@gmail.com>
2024-09-23 17:41:38 +0200

    fix role

---
+++ lib/services/profile_service/profileService.dart
@@ -0,0 +1,35 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class ProfileService {
+  static Future<Map<String, dynamic>> getUserProfile(String userId) async {
+    DocumentSnapshot userSnapshot = await FirebaseFirestore.instance
+        .collection('users')
+        .doc(userId)
+        .get();
+
+    return {
+      'name': userSnapshot['name'] ?? '',
+      'surname': userSnapshot['surname'] ?? '',
+      'birthdate': userSnapshot['birthdate'] ?? '',
+      'addressUser': userSnapshot['addressUser'] ?? '',
+      'phoneNumber': userSnapshot['phoneNumber'] ?? '',
+      'fidelity': userSnapshot['fidelity']?.toString() ?? '0',
+    };
+  }
+
+  static Future<Map<String, dynamic>> getActivityProfile(String userId) async {
+    DocumentSnapshot activitySnapshot = await FirebaseFirestore.instance
+        .collection('activities') // Adjust the collection name as needed
+        .doc(userId)
+        .get();
+
+    return {
+      'name': activitySnapshot['name'] ?? '',
+      'type': activitySnapshot['type'] ?? '', // Include activity type if needed
+      'description': activitySnapshot['description'] ?? '',
+      'contacts': activitySnapshot['contacts'] ?? '',
+      'addressActivity': activitySnapshot['addressActivity'] ?? '',
+      'fidelity': activitySnapshot['fidelity'] ?? 0, // Default to 0 if not set
+    };
+  }
+}
