816b23b72a4ddf7adb18f2837b8a5b74090aef26
andrea <andreagazze98@gmail.com>
2024-10-30 18:20:13 +0100

    fix create service

--- lib/pages/geolocation/find_location_page.dart
+++ lib/pages/geolocation/find_location_page.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:geocoding/geocoding.dart';
 import 'package:geolocator/geolocator.dart';
 
+import '../../services/findLocation_service/FindLocationService.dart';
 import '../home_page/home_page.dart';
 
 class FindLocationPage extends StatefulWidget {
@@ -18,26 +19,22 @@ class _FindLocationPageState extends State<FindLocationPage> {
   String? searchedLocation;
   Position? _searchedPosition;
   List<String> _suggestions = [];
+  final FindLocationService _findLocationService = FindLocationService();
 
   Future<void> _searchLocation(String query) async {
     try {
-      // Ottieni le coordinate dalla query dell'indirizzo
       List<Location> locations = await locationFromAddress(query);
 
-      // Se trovi almeno una posizione
       if (locations.isNotEmpty) {
-        // Converti le coordinate in nomi di posizioni leggibili
         List<Placemark> placemarks = await placemarkFromCoordinates(
           locations[0].latitude,
           locations[0].longitude,
         );
 
-        // Aggiungi la prima corrispondenza
         List<String> suggestions = placemarks
             .map((place) => '${place.street}, ${place.locality}, ${place.administrativeArea}, ${place.country}')
             .toList();
 
-        // Controlla se il widget è ancora montato prima di chiamare setState
         if (mounted) {
           setState(() {
             _suggestions = suggestions;
@@ -51,59 +48,11 @@ class _FindLocationPageState extends State<FindLocationPage> {
     } catch (e) {
       print("Errore nella ricerca della posizione: $e");
 
-      // Controlla se il widget è ancora montato prima di chiamare setState
       if (mounted) {
         setState(() {
-          _suggestions = [];  // Pulisci i suggerimenti in caso di errore
+          _suggestions = [];
         });
       }
-    }
-  }
-
-  Future<void> _addLocation(String locationName, Position position) async {
-    final User? user = FirebaseAuth.instance.currentUser;
-
-    if (user != null) {
-      final userPlacesDoc = FirebaseFirestore.instance.collection('places').doc(user.uid);
-
-      final docSnapshot = await userPlacesDoc.get();
-
-      // Definisci la nuova posizione da aggiungere
-      Map<String, dynamic> newLocation = {
-        'name': locationName,  // Nome della posizione selezionata
-        'latitude': position.latitude,  // Latitudine della posizione selezionata
-        'longitude': position.longitude,  // Longitudine della posizione selezionata
-      };
-
-      if (docSnapshot.exists) {
-        // Se il documento esiste, ottieni l'array esistente
-        Map<String, dynamic> data = docSnapshot.data() as Map<String, dynamic>;
-        List<dynamic> placesList = data['placesList'] ?? [];
-
-        // Inserisci la nuova posizione nella seconda posizione (index 1), dopo la posizione corrente
-        if (placesList.length >= 1) {
-          placesList.insert(1, newLocation);  // Aggiungi la nuova posizione alla lista
-        } else {
-          // Se l'elenco è vuoto, aggiungi la nuova posizione
-          placesList.add(newLocation);
-        }
-
-        // Aggiorna il documento con la lista aggiornata
-        await userPlacesDoc.update({
-          'placesList': placesList,
-        });
-      } else {
-        // Se il documento non esiste, crealo con un array di placesList e includi il campo name
-        await userPlacesDoc.set({
-          'userId': user.uid,  // ID dell'utente
-          'placesList': [
-            newLocation,  // Prima posizione aggiunta all'array
-          ],
-        });
-      }
-
-      // Ritorna alla schermata precedente
-      Navigator.of(context).pop(locationName);
     }
   }
 
@@ -117,7 +66,7 @@ class _FindLocationPageState extends State<FindLocationPage> {
         elevation: 0,
       ),
       body: Container(
-        color: Colors.white, // Sfondo bianco
+        color: Colors.white,
         child: Padding(
           padding: const EdgeInsets.all(16.0),
           child: Column(
@@ -126,13 +75,13 @@ class _FindLocationPageState extends State<FindLocationPage> {
               TextField(
                 controller: _searchController,
                 onChanged: (query) {
-                  _searchLocation(query);  // Aggiorna i suggerimenti mentre l'utente scrive
+                  _searchLocation(query);
                 },
                 decoration: InputDecoration(
                   hintText: 'Inserisci un indirizzo o una via...',
                   prefixIcon: const Icon(Icons.search, color: Colors.blueAccent),
                   filled: true,
-                  fillColor: Colors.grey[200], // Colore leggero per il campo di testo
+                  fillColor: Colors.grey[200],
                   border: OutlineInputBorder(
                     borderRadius: BorderRadius.circular(30),
                     borderSide: BorderSide.none,
@@ -147,7 +96,7 @@ class _FindLocationPageState extends State<FindLocationPage> {
                   itemCount: _suggestions.length,
                   itemBuilder: (context, index) {
                     return Card(
-                      color: Colors.grey[200], // Colore del Card
+                      color: Colors.grey[200],
                       margin: const EdgeInsets.symmetric(vertical: 10),
                       child: ListTile(
                         leading: const Icon(Icons.location_on, color: Colors.blueAccent),
@@ -156,7 +105,6 @@ class _FindLocationPageState extends State<FindLocationPage> {
                           style: const TextStyle(color: Colors.black87),
                         ),
                         onTap: () async {
-                          // Quando l'utente seleziona un suggerimento, ottieni le coordinate
                           List<Location> locations = await locationFromAddress(_suggestions[index]);
                           Position selectedPosition = Position(
                             latitude: locations[0].latitude,
@@ -167,17 +115,14 @@ class _FindLocationPageState extends State<FindLocationPage> {
                             heading: 0.0,
                             speed: 0.0,
                             speedAccuracy: 0.0,
-                            headingAccuracy: 0.0,
                             altitudeAccuracy: 0.0,
+                            headingAccuracy: 0.0,
                           );
 
-                          // Aggiungi la posizione selezionata automaticamente e torna alla schermata precedente
-                          await _addLocation(_suggestions[index], selectedPosition);
+                          await _findLocationService.addLocation(_suggestions[index], selectedPosition);
                           Navigator.pushReplacement(
                             context,
-                            MaterialPageRoute(
-                              builder: (context) => const HomePage(), // Torna alla home
-                            ),
+                            MaterialPageRoute(builder: (context) => const HomePage()),
                           );
                         },
                       ),
